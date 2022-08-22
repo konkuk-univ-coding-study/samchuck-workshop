2a2ef1fdea96ab4f240b370abbf5aa55568eac3f
kyuhyeongkwon <akcps1tp@naver.com>
2022-08-22 18:11:33 +0900

    add: user service

---
+++ src/main/java/konkuk/samchuck/service/UserService.java
@@ -0,0 +1,21 @@
+package konkuk.samchuck.service;
+
+import konkuk.samchuck.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public boolean isDuplicate(String id) {
+
+    }
+}
