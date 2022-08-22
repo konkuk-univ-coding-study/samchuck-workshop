6ec30401b540f0e58f50ed90081778327555cc7e
kyuhyeongkwon <akcps1tp@naver.com>
2022-08-22 23:53:28 +0900

    add: api Response type

---
+++ src/main/java/konkuk/samchuck/response/Response.java
@@ -0,0 +1,21 @@
+package konkuk.samchuck.response;
+
+public class Response {
+
+    private final String code;
+
+    private final String message;
+
+    public Response(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
