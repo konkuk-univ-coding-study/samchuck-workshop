f2dad6404368fc3cf6a6a5046b16d0686146058d
kyuhyeongkwon <akcps1tp@naver.com>
2022-08-22 15:39:43 +0900

    add: initial commit

---
+++ src/main/java/konkuk/samchuck/SamchuckApplication.java
@@ -0,0 +1,13 @@
+package konkuk.samchuck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamchuckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamchuckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/konkuk/samchuck/SamchuckApplicationTests.java
@@ -0,0 +1,13 @@
+package konkuk.samchuck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamchuckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
