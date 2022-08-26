b789a480fb0a555e323513d7af5a5f95ec428fcf
kyuhyeongkwon <akcps1tp@naver.com>
2022-08-26 21:51:28 +0900

    feat: 로그인

--- src/main/java/konkuk/samchuck/service/UserService.java
+++ src/main/java/konkuk/samchuck/service/UserService.java
@@ -2,18 +2,25 @@ package konkuk.samchuck.service;
 
 import konkuk.samchuck.domain.User;
 import konkuk.samchuck.repository.UserRepository;
-import konkuk.samchuck.util.SHA256Util;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+
 @Service
 @Transactional
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
@@ -26,14 +33,27 @@ public class UserService {
 
     @Transactional
     public void signup(User user) {
-        String encoded = encodePassword(user);
-        user.setPassword(encoded);
+        checkDuplicate(user.getUserId());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setActive(true);
         userRepository.save(user);
     }
 
-    private String encodePassword(User user) {
-        String original = user.getPassword();
-        String salt = SHA256Util.generateSalt();
-        return SHA256Util.getEncrypt(original, salt);
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userid) throws UsernameNotFoundException {
+        return userRepository.findByUserid(userid)
+                .map(user -> createUser(userid, user))
+                .orElseThrow(() -> new UsernameNotFoundException(userid + " -> 존재하지 않는 유저!!"));
+    }
+
+    private org.springframework.security.core.userdetails.User createUser(String userid, User user) {
+        if (!user.isActive()) {
+            throw new RuntimeException(userid + " -> 활성화되지 않은 유저!!");
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getUserId(),
+                user.getPassword(),
+                new ArrayList<>());
     }
 }
