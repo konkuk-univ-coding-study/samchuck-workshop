cfb0dbc6e71799b26e54798b537518556a0c48cc
kyuhyeongkwon <akcps1tp@naver.com>
2022-08-29 15:42:07 +0900

    feat: jwt interceptor 등록

--- src/main/java/konkuk/samchuck/config/WebConfig.java
+++ src/main/java/konkuk/samchuck/config/WebConfig.java
@@ -1,16 +1,31 @@
 package konkuk.samchuck.config;
 
+import konkuk.samchuck.interceptors.BearerAuthInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final BearerAuthInterceptor bearerAuthInterceptor;
+
+    public WebConfig(BearerAuthInterceptor bearerAuthInterceptor) {
+        this.bearerAuthInterceptor = bearerAuthInterceptor;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(bearerAuthInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/api/signup/**", "/api/authenticate/**");
     }
 }
