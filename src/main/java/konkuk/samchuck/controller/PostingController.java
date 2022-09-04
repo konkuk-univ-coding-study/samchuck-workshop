2d55317baf4d7825400570a386e1728e79da8f08
kyuhyeongkwon <akcps1tp@naver.com>
2022-09-05 00:39:37 +0900

    feat: 게시글 목록 조회 Controller

--- src/main/java/konkuk/samchuck/controller/PostingController.java
+++ src/main/java/konkuk/samchuck/controller/PostingController.java
@@ -2,20 +2,18 @@ package konkuk.samchuck.controller;
 
 import konkuk.samchuck.domain.Posting;
 import konkuk.samchuck.domain.User;
-import konkuk.samchuck.dto.PostingDTO;
-import konkuk.samchuck.dto.ResponseDTO;
+import konkuk.samchuck.dto.*;
 import konkuk.samchuck.service.PostingService;
 import konkuk.samchuck.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/board")
+@RequestMapping("/api/posting")
 public class PostingController {
 
     private final UserService userService;
@@ -26,6 +24,7 @@ public class PostingController {
         this.postingService = postingService;
     }
 
+    @PostMapping("/create")
     public ResponseEntity<ResponseDTO> savePosting(HttpServletRequest request, @RequestBody PostingDTO postingDTO) {
         Posting posting = new Posting();
         String username = (String) request.getAttribute("username");
@@ -37,5 +36,14 @@ public class PostingController {
         postingService.createPost(posting);
         return new ResponseEntity<>(
                 new ResponseDTO("success", "created new posting"), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseAllPostingsDTO> getAllPostings(@RequestParam("page") int page) {
+        ResponseAllPostingsDTO responseAllPostingsDTO =
+                new ResponseAllPostingsDTO("success", "loaded all postings");
+        List<Posting> postings = postingService.getPostings(page);
+        postings.forEach(posting -> responseAllPostingsDTO.getPostings().add(new PostingInfoDTO(posting)));
+        return new ResponseEntity<>(responseAllPostingsDTO, HttpStatus.OK);
     }
 }
